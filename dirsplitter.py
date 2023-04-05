5aa9ece6edab61585dfaa6346b9127f9e0ddf0fa
jinyus <starboymc4@gmail.com>
2023-04-05 00:42:41 -0500

    user typer

--- dirsplitter.py
+++ dirsplitter.py
@@ -1,31 +1,29 @@
 import shutil
-import click
+import typer
 from pathlib import Path
 import re
 import traceback
 
 APP_VERSION = '1.0.0'
 
-
-@click.group()
-def cli():
-    pass
+app = typer.Typer(add_completion=False)
 
 
-@click.command()
+@app.command()
 def version():
-    click.echo('Dirsplitter Version: %s' % APP_VERSION)
+    typer.echo('Dirsplitter Version: %s' % APP_VERSION)
 
 
-@click.command()
-@click.argument(
-    'directory', default='.',
-    type=click.Path(exists=True, file_okay=False, resolve_path=True),
-)
-@click.option('-m', '--max', default=5.0, help='Size of each part in GB', type=click.FLOAT, show_default=True)
-@click.option('-p', '--prefix', default='', help='Prefix for output files of the tar command. eg: myprefix.part1.tar')
-def split(directory: Path, max: float, prefix: str):
-    click.confirm(
+@app.command(help='Split a directory into parts of a given size')
+def split(
+    directory: Path = typer.Argument(
+        '.', exists=True, file_okay=False, resolve_path=True),
+    max: float = typer.Option(
+        5.0, '-m', '--max', help='Size of each part in GB', show_default=True),
+    prefix: str = typer.Option(
+        '', help='Prefix for output files of the tar command. eg: myprefix.part1.tar')
+):
+    typer.confirm(
         f'Split "{directory}" into parts of {max} GB', abort=True, default=True)
 
     tracker = {1: 0}
@@ -33,12 +31,12 @@ def split(directory: Path, max: float, prefix: str):
     files_moved = 0
     failed_ops = 0
 
-    all_files = Path(directory).rglob('*')
+    all_files = directory.rglob('*')
 
     # moves ./dir/textfiles/file.txt to ./dir/part1/textfiles/file.txt
 
     max_size = max * 1024 * 1024 * 1024
-    part_dir_regex = re.compile(str(Path(directory).joinpath('part\d+')))
+    part_dir_regex = re.compile(str(directory.joinpath('part\d+')))
     for f in all_files:
         if f.is_dir() or part_dir_regex.findall(str(f.absolute())):
             continue
@@ -50,7 +48,7 @@ def split(directory: Path, max: float, prefix: str):
                 current_part += 1
                 tracker[current_part] = 0
 
-            part_dir = Path(directory).joinpath(f'part{current_part}')
+            part_dir = directory.joinpath(f'part{current_part}')
             new_path = part_dir.joinpath(f.relative_to(directory))
             new_path.parent.mkdir(exist_ok=True, parents=True)
 
@@ -58,44 +56,48 @@ def split(directory: Path, max: float, prefix: str):
 
             files_moved += 1
         except Exception as e:
-            click.echo('Failed to move file: %s' % f)
-            click.echo(e)
+            typer.echo('Failed to move file: %s' % f)
+            typer.echo(e)
             print(traceback.format_exc())
 
             failed_ops += 1
 
-    click.echo('Results:')
-    click.echo(f'files moved: {files_moved}')
-    click.echo(f'failed operations: {failed_ops}')
+    typer.echo('Results:')
+    typer.echo(f'files moved: {files_moved}')
+    typer.echo(f'failed operations: {failed_ops}')
 
     # print the size of each part
     for part in tracker:
-        click.echo(f'part{part}: {int(tracker[part] / 1024 / 1024)}MB')
+        typer.echo(f'part{part}: {int(tracker[part] / 1024 / 1024)}MB')
 
     if prefix != '' and current_part > 0:
-        click.echo('Tar Commands:')
+        typer.echo('Tar Commands:')
         if current_part == 1:
-            click.echo(
+            typer.echo(
                 f'tar -cf "{prefix}part1.tar" "part1"; done')
         else:
-            click.echo(
-                f'for n in 1..{current_part}; do tar -cf "{prefix}part$n.tar" "part$n"; done')
+            typer.echo(
+                'for n in {1..%d}; do tar -cf "%spart$n.tar" "part$n"; done' % (current_part, prefix))
 
 
-@click.command()
-@click.argument(
-    'directory', default='.',
-    type=click.Path(exists=True, file_okay=False, resolve_path=True),
-)
-def reverse(directory: Path):
-    # moves ./dir/part1/textfiles/file.txt to ./dir/textfiles/file.txt
+@app.command(help='Reverse a split directory')
+def reverse(
+    directory: Path = typer.Argument(
+        '.',
+        exists=True,
+        file_okay=False,
+        resolve_path=True,
+    ),
+
+):
+
     folders_to_remove: set[str] = set()
 
-    click.confirm(f'Reverse split "{directory}"?', abort=True, default=True)
+    typer.confirm(f'Reverse split "{directory}"?', abort=True, default=True)
     should_delete = True
 
     try:
-        for item in Path(directory).iterdir():
+        for item in directory.iterdir():
             if item.is_dir() and re.match(r'part\d+', item.name):
                 files = item.rglob('*')
 
@@ -103,13 +105,14 @@ def reverse(directory: Path):
                     if f.is_dir():
                         continue
 
-                    new_path = Path(directory).joinpath(f.relative_to(item))
+                    new_path = directory.joinpath(
+                        f.relative_to(item))
                     f.rename(new_path)
 
                 folders_to_remove.add(item)
     except Exception as e:
-        click.echo(f'Failed to move file')
-        click.echo(e)
+        typer.echo(f'Failed to move file')
+        typer.echo(e)
         should_delete = False
 
     if should_delete:
@@ -117,14 +120,9 @@ def reverse(directory: Path):
             try:
                 shutil.rmtree(folder)
             except Exception as e:
-                click.echo(f'Failed to remove folder: {folder}')
-                click.echo(e)
-
-
-cli.add_command(version)
-cli.add_command(split)
-cli.add_command(reverse)
+                typer.echo(f'Failed to remove folder: {folder}')
+                typer.echo(e)
 
 
 if __name__ == '__main__':
-    cli()
+    app()
